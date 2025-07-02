6fe0619818f7497d9c743de113d634d64972c948
akash@stockedge.com <akash@stockedge.com>
2025-07-02 12:54:14 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'population_gdp_dashboard',
+  webDir: 'www'
+};
+
+export default config;
